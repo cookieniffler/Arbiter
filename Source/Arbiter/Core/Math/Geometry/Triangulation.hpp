97d1c1ef8af912891acbe72e0149ba41131c81ea
Kapil Gupta <kpgupta98@gmail.com>
2019-07-17 08:53:07 +0530

    boilerplate

---
+++ Source/Arbiter/Core/Math/Geometry/Triangulation.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef __TRIANGULATION_HPP__
+#define __TRIANGULATION_HPP__
+
+#include <Arbiter/Core/Common/Base.hpp>
+
+ARBITER_NAMESPACE_BEGIN
+
+ARBITER_NAMESPACE_END
+
+#endif  // __TRIANGULATION_HPP__

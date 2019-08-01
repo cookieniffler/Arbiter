319edbb14898b4d209c2a0f38fb1ff531cd44959
Kapil Gupta <kpgupta98@gmail.com>
2019-08-01 19:41:26 +0530

    some moving around

---
+++ Source/Arbiter/RayTracing/RayTracing.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#ifndef __RAYTRACING_HPP__
+#define __RAYTRACING_HPP__
+
+#include <Arbiter/Core/Common/Base.hpp>
+
+ARBITER_NAMESPACE_BEGIN
+
+ARBITER_NAMESPACE_END
+
+#endif  // __RAYTRACING_HPP__
